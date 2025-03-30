6e4f610a6fcae81fd8ff5c705837cb0af6c1477f
averma-akash <averma.akash@gmail.com>
2025-03-30 16:11:48 +0530

    Added Authentication code
    Authentication code added

---
+++ src/main/java/com/example/notification/security/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.example.notification.security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.notification.repository.UserRepository;
+import com.example.notification.service.AuthService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private final JwtUtil jwtUtils;
+
+	public JwtAuthenticationFilter(JwtUtil jwtUtils, UserRepository userDao, AuthService userService) {
+		this.jwtUtils = jwtUtils;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		try {
+			String jwt = parseJwt(request);
+			if (null != jwt) {
+				String username = jwtUtils.extractUsername(jwt);
+				if (null != username && jwtUtils.validateJwtToken(jwt, username)) {
+					UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+							username, null, null);
+					authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					SecurityContextHolder.getContext().setAuthentication(authentication);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Cannot set user authentication: {}", e);
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private String parseJwt(HttpServletRequest request) {
+		String jwt = jwtUtils.getJwtFromCookie(request);
+		return jwt;
+	}
+
+}
